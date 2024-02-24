d6be2386e4e09be782a9742a92937c061b17a2d2
Ali Ayaz <160322443+Ali27Ayaz@users.noreply.github.com>
2024-02-24 14:14:21 +0500

    Add files via upload

---
+++ lesson2.js
@@ -0,0 +1,13 @@
+// Asslam O Alaikum
+var fname = "My Name is: Ayaz Ali";
+var Surname = "Shahani";
+var Coursename = "Typescript";
+var Remarks = "I really enjoying this Course, thanx to my Teacher's who taught me well";
+console.log();
+console.log(fname + " " + Surname);
+console.log();
+console.log("& My Course Name is " + Coursename);
+console.log();
+console.log(Remarks);
+var person_name = "ali ayaz";
+console.log(person_name.charAt(0).toUpperCase);
---
+++ lesson2.ts
@@ -0,0 +1,13 @@
+// Asslam O Alaikum
+let fname= "My Name is: Ayaz Ali"
+let Surname= "Shahani"
+let Coursename= "Typescript"
+let Remarks= "I really enjoying this Course, thanx to my Teacher's who taught me well"
+console.log(   )
+console.log(fname + " " + Surname)
+console.log(   )
+console.log("& My Course Name is " + Coursename)
+console.log(   )
+console.log(Remarks)
+
+
